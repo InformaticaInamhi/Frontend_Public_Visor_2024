04ecd5513ea6c616f585b5bfc1a61cece85cc126
Kevin Changoluisa <46755578+KevinChangoluisa@users.noreply.github.com>
2024-06-28 16:36:07 -0500

    update routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,18 +4,8 @@ import { EstacionesComponent } from './components/map/estaciones/estaciones.comp
 import { GuiasVisorComponent } from './components/basic/guias-visor/guias-visor.component';
 
 export const routes: Routes = [
-  { path: '', redirectTo: 'visor', pathMatch: 'full' },
+  { path: '', redirectTo: '/visor', pathMatch: 'full' },
+  { path: 'visor', component: EstacionesComponent, title: 'Visor de Estaciones' },
   { path: 'login', component: LoginComponent, title: 'Login' },
-  { path: 'login', component: LoginComponent, title: 'Login' },
-  {
-    path: 'repositorio',
-    component: GuiasVisorComponent,
-    title: 'repositorio',
-  },
-
-  {
-    path: 'visor',
-    component: EstacionesComponent,
-    title: 'visor de estaciones',
-  },
+  { path: 'repositorio', component: GuiasVisorComponent, title: 'Repositorio' },
 ];
